e611a931eb198a83b15c6802c99f3dafb9332a6c
Gabriel.Borges <gabriel.borges@useall.com.br>
2023-11-23 12:36:20 -0300

    Requisição post para login de acesso

--- requires-users.js
+++ requires-users.js
@@ -2,8 +2,12 @@ import { randomUUID } from 'node:crypto'
 import { sql } from './db.js';
 
 export class RequiresUsers {
-    list(filters) {
-        
+    async readLogin(dto) {
+        const { email, password } = dto;
+
+        const res = await sql` select COUNT(*) as total from users where email = ${email} and password = ${password}`;
+
+        return res[0].total;
     };
 
     async create(dto) {
@@ -11,7 +15,6 @@ export class RequiresUsers {
         const { username, date, email, password } = dto;
 
         await sql` insert into users (id, username, date, email, password) VALUES (${userId}, ${username}, ${date}, ${email}, ${password})`;
-
     };
 
     update(id, dto) {
--- server.js
+++ server.js
@@ -25,6 +25,21 @@ server.post('/signin/create', async (resquest, reply) => {
     return reply.status(201).send();
 });
 
+server.post('/login/acesso', async (request, reply) => {
+    const { email, password } = request.body;
+
+    const respSql = await database.readLogin({
+        email,
+        password
+    });
+
+    if (respSql > 0) {
+        reply.status(200).send({ success: true, message: 'Login bem-sucedido!' });
+    } else {
+        reply.status(401).send({ success: false, message: 'Credenciais inválidas.' });
+    }
+});
+
 server.listen({
     port: 3333
 });
